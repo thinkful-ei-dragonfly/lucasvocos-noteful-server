1e9efef45a99aece12662657442500ac8432268f
Lucas Vocos <lucas.vocos@me.com>
2019-06-22 09:01:38 -0400

    update server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgres://lucasvocos@localhost/noteful'
+  DB_URL: process.env.DATABASE_URL || 'postgres://lucasvocos@localhost/noteful'
 }
